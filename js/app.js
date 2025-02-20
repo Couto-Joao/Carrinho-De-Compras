430765594b525715ff6438de860c82f0b9a2d65d
João Pedro Gonçalves Couto <rustincouto@gmail.com>
2025-02-20 19:13:09 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+let somaTotal;
+limpar();
+
+function adicionar() {
+  let produtos = document.getElementById('produto').value;
+  let nomeProduto = produtos.split('-')[0];
+  let valorProduto = produtos.split('R$')[1];
+  let quantidadeProduto = document.getElementById('quantidade').value;
+  //Cálculo do subtotal
+  let subtotal = (quantidadeProduto * valorProduto);
+    
+  //Adicionando no carrinho
+  let carrinho = document.getElementById('lista-produtos');
+  carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+  <span class="texto-azul">${quantidadeProduto}x</span> ${nomeProduto} <span class="texto-azul">R$${subtotal}</span>
+  </section>`;
+
+  //Atualizando valor total
+  somaTotal = somaTotal + subtotal;
+  let valorTotal = document.getElementById('valor-total');
+  valorTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${somaTotal}</span>`;
+    
+  document.getElementById('quantidade').value = '';
+}
+
+function limpar() {
+  somaTotal = 0;
+  document.getElementById('lista-produtos').innerHTML = '';
+  document.getElementById('valor-total').textContent = 'R$';
+  document.getElementById('quantidade').value = '';
+}
+
